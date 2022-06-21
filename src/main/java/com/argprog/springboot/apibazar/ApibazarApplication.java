ff2071108f60ae7cc26ad21a6a07bea517dcabf7
maxpipoka <martincardozo@gmail.com>
2022-06-20 21:05:04 -0300

    First commit

---
+++ src/main/java/com/argprog/springboot/apibazar/ApibazarApplication.java
@@ -0,0 +1,13 @@
+package com.argprog.springboot.apibazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibazarApplication.class, args);
+	}
+
+}
